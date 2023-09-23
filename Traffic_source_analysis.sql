5bf84d93f96bc7d2872820eb5bba64dea030b96c
Sonukumarsingh <sonusingham0000@gmail.com>
2023-09-23 14:26:59 +0530

    Calculated conv_rate

--- Traffic_source_analysis.sql
+++ Traffic_source_analysis.sql
@@ -17,3 +17,33 @@ from website_sessions
  where created_at <'2012-04-12'
  group by 1,2,3
  order by sessions desc;
+
+ /*   conversion rate */
+ 
+ /* From last quiry we get to know that utm_source=gsearch 
+ and utm_campaign= nonbrand creating all the traffic.
+ In this step will calculate the conversion rate for that utm_source and utm_campaign.
+ date limit 2012-04-14 */
+ 
+ select count(distinct website_sessions.website_session_id) as sessions,
+        count(distinct orders.order_id) as orders,
+        count(distinct orders.order_id)/
+        count(distinct website_sessions.website_session_id) as conver_rate
+        from website_sessions
+        left join orders
+        on website_sessions.website_session_id=orders.website_session_id
+        where website_sessions.created_at <'2012-04-14' 
+		and utm_source='gsearch'
+        and utm_campaign='nonbrand';
+
+
+
+
+
+
+
+
+
+
+
+

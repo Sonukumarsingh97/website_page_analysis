5ccc5375013ff2e02648412c5fa9316b2054a961
Sonukumarsingh <sonusingham0000@gmail.com>
2023-09-27 17:39:39 +0530

    Top entry pages

--- Analyzing_website_performance.sql
+++ Analyzing_website_performance.sql
@@ -12,3 +12,29 @@ select count(distinct website_pageview_id) as pageview,
       where created_at < '2012-06-09' 
       group by pageview_url
       order by pageview desc;
+
+
+ /* finding top entey pages date limit 2012-06-12 */
+
+/* step 1 finding min_pageview
+   step 2 find the url the coustomer saw on thst min_pageview */
+create temporary table min_pageviews
+SELECT 
+    website_session_id,
+    MIN(website_pageview_id) AS min_pageview_id
+FROM
+    website_pageviews
+WHERE
+    created_at < '2012-06-12'
+GROUP BY website_session_id;
+      
+      
+SELECT 
+    website_pageviews.pageview_url AS landing_page,
+    COUNT(DISTINCT min_pageviews.website_session_id) AS sessions
+FROM
+    min_pageviews
+        LEFT JOIN
+    website_pageviews ON website_pageviews.website_pageview_id = min_pageviews.min_pageview_id
+GROUP BY landing_page
+ORDER BY sessions DESC;     

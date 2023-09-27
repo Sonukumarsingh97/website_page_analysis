99a238cf9c518c91e21afd17fdf1f5d07cbf369c
Sonukumarsingh <sonusingham0000@gmail.com>
2023-09-27 18:00:50 +0530

    Bounce_rate_analysis

--- Analyzing_website_performance.sql
+++ Analyzing_website_performance.sql
@@ -37,4 +37,69 @@ FROM
         LEFT JOIN
     website_pageviews ON website_pageviews.website_pageview_id = min_pageviews.min_pageview_id
 GROUP BY landing_page
-ORDER BY sessions DESC;     
+ORDER BY sessions DESC;  
+
+
+
+/* web_page bounce rate analysis */
+
+/* step 1 find the first_pageview_id for relevent sessions
+   step 2 identify the landing page for each session
+   step 3 counting the pageview for each session to identify the bounced session 
+   step 4 summerizing the count of session and bounced session */
+
+
+/* step 1 find the first pageview id for relevent session*/
+
+create temporary table first_pageviews
+select website_pageviews.website_session_id,
+min(website_pageviews.website_pageview_id) as min_pageview_id
+from website_pageviews
+where created_at < '2012-06-14' 
+group by website_pageviews.website_session_id;
+
+
+/* step 2 identify the landing page for each session*/
+
+
+create temporary table sessions_w_home_landing_page
+SELECT 
+    first_pageviews.website_session_id,
+    website_pageviews.pageview_url AS landing_page
+FROM
+    first_pageviews
+        LEFT JOIN
+    website_pageviews ON website_pageviews.website_session_id = first_pageviews.website_session_id
+WHERE
+    website_pageviews.pageview_url = '/home'; 
+
+    
+/*  step 3 counting the pageview for each session to identify the bounced session */
+
+
+create temporary table bounced_sessions     
+SELECT 
+    sessions_w_home_landing_page.website_session_id,
+    sessions_w_home_landing_page.landing_page,
+    COUNT(website_pageviews.website_pageview_id) AS count_of_pageview
+FROM
+    sessions_w_home_landing_page
+        LEFT JOIN
+    website_pageviews ON sessions_w_home_landing_page.website_session_id = website_pageviews.website_session_id
+GROUP BY sessions_w_home_landing_page.website_session_id , sessions_w_home_landing_page.landing_page
+HAVING COUNT(website_pageviews.website_pageview_id) = 1;
+       
+       
+  /* step 4 summerizing the count of session and bounced session */
+     
+       
+  SELECT 
+    sessions_w_home_landing_page.landing_page AS landing_page,
+    COUNT(DISTINCT sessions_w_home_landing_page.website_session_id) AS sessions,
+    COUNT(DISTINCT bounced_sessions.website_session_id) AS bounced_sessions,
+    COUNT(DISTINCT bounced_sessions.website_session_id) / COUNT(DISTINCT sessions_w_home_landing_page.website_session_id) AS bounce_rate
+FROM
+    sessions_w_home_landing_page
+        LEFT JOIN
+    bounced_sessions ON sessions_w_home_landing_page.website_session_id = bounced_sessions.website_session_id
+GROUP BY landing_page;
